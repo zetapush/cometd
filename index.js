f2747ef3165409b5f3bfa86d5226415c685f1c6f
ghoullier <gregory.houllier@gmail.com>
2016-03-22 10:19:43 +0100

    chore(init) Add cometd with full CommonJS support

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  CallbackPollingTransport: require('./lib/CallbackPollingTransport'),
+  CometD: require('./lib/CometD'),
+  LongPollingTransport: require('./lib/LongPollingTransport'),
+  RequestTransport: require('./lib/RequestTransport'),
+  Transport: require('./lib/Transport'),
+  TransportRegistry: require('./lib/TransportRegistry'),
+  Utils: require('./lib/Utils'),
+  WebSocketTransport: require('./lib/WebSocketTransport')
+}
