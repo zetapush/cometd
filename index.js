6ec7cd1f5f9a0c3c8ba6f68e3c9cd7bf6b859411
Grégory Houllier <gregory.houllier@gmail.com>
2017-05-29 13:23:43 +0200

    feat(node-env) Improve node env support with transports layer

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-module.exports = {
-  CallbackPollingTransport: require('./lib/CallbackPollingTransport'),
-  CometD: require('./lib/CometD'),
-  LongPollingTransport: require('./lib/LongPollingTransport'),
-  RequestTransport: require('./lib/RequestTransport'),
-  Transport: require('./lib/Transport'),
-  TransportRegistry: require('./lib/TransportRegistry'),
-  Utils: require('./lib/Utils'),
-  WebSocketTransport: require('./lib/WebSocketTransport')
-}
+module.exports.CometD = require('./lib/CometD')
+module.exports.Transports = require('./lib/browser/Transports')
